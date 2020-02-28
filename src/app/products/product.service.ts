6082c5c2386eed63951e1fdd85eb5fe99d6ab6c7
beeeg55 <beeeg@naver.com>
2020-02-27 20:06:46 -0500

    connect database as lambda

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,39 +1,46 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "./Products";
-import { Observable, throwError } from 'rxjs';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, of } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { map, tap, catchError } from 'rxjs/operators';
+
+
 
 @Injectable({
     providedIn: 'root'
 })
 
+
 export class ProductService {
-    private productUrl: string = 'api/products.json';
+    
+    private productUrl: string = 'https://f7vxewxmz9.execute-api.us-east-1.amazonaws.com/products/getproduct';
 
     constructor(private http: HttpClient) {}
+    
     getProducts() : Observable<IProduct[]> {
         return this.http.get<IProduct[]>(this.productUrl).pipe(
-            tap(data => console.log( JSON.stringify(data) )),
-            catchError(this.handleError)
+        tap(data => console.log( JSON.stringify(data) )),
+        catchError(this.handleError)
         );
     }
     
     getProduct(id: number): Observable<IProduct | undefined> {
         return this.getProducts().pipe(
-          map((products: IProduct[]) => products.find(p => p.productId === id))
+         map(
+            (products: IProduct[]) => products.find(p => p.productId === id)
+         )
         );
-      }
-    
+    } 
 
-    private handleError(err: HttpErrorResponse) {
-        let errMessage = '';
-        if(err.error instanceof ErrorEvent){
-            errMessage = `An error occured: ${err.error.message}`;
-        } else{
-            errMessage = `Server returned code: ${err.status}, error message is ${err.message}`;
-        }
-        console.error(errMessage);
-        return throwError(errMessage);
+private handleError(err: HttpErrorResponse){
+    let errMessage = '';
+    if(err.error instanceof ErrorEvent){
+        errMessage = `An error occurred: ${err.error.message}`;
+    } else {
+        errMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
     }
+
+    console.error(errMessage);
+    return throwError(errMessage);
+}
 }
